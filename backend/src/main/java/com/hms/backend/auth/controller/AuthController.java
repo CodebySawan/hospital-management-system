4f25743d19c3ba73d492fa75d1cb87de709698ce
CodebySawan <work.sawanpandey@gmail.com>
2026-05-26 13:47:25 +0530

    Completed user registration flow

---
+++ backend/src/main/java/com/hms/backend/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.hms.backend.auth.controller;
+
+import com.hms.backend.auth.dto.RegisterRequest;
+import com.hms.backend.auth.dto.RegisterResponse;
+import com.hms.backend.auth.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    // Constructor Injection
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public RegisterResponse register(
+            @Valid @RequestBody RegisterRequest request
+    ) {
+
+        return authService.register(request);
+    }
+}
---
+++ backend/src/main/java/com/hms/backend/auth/service/AuthService.java
@@ -0,0 +1,46 @@
+package com.hms.backend.auth.service;
+
+import com.hms.backend.auth.dto.RegisterRequest;
+import com.hms.backend.auth.dto.RegisterResponse;
+import com.hms.backend.auth.entity.User;
+import com.hms.backend.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterResponse register(RegisterRequest request) {
+
+        // check if email already exists
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        // create user object
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        // save user
+        userRepository.save(user);
+
+        // return response
+        return RegisterResponse.builder()
+                .message("User registered successfully")
+                .email(user.getEmail())
+                .role(user.getRole().name())
+                .build();
+    }
+}
